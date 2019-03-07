73eafbf7ade1aa545472607aa35fdf77def536c3
kobajanashvili <kobajanashvili@gmail.com>
2019-03-07 13:54:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const path = require('path')
-const postRouter = require('./routes/post')
-const keys = require('./keys')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const postRouter = require('./routes/post');
+const keys = require('./keys');
 
-const port = process.env.PORT || 5000
-const clientPath = path.join(__dirname, 'client')
+const port = process.env.PORT || 5000;
+const clientPath = path.join(__dirname, 'client');
 
 // ********** connect MongoDB ************
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
@@ -14,17 +14,18 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true })
     .catch(err => console.error(err))
 // ********** end connect MongoDB ************
 
-const app = express()
-app.use(bodyParser.json())
-app.use('/api/post', postRouter)
-app.use(express.static(clientPath))
+const app = express();
+app.use(bodyParser.json());
+app.use('/api/post', postRouter);
+
+app.use(express.static(clientPath));
 
 
 
 
 
 app.listen(port, () => {
-    console.log(`Server has been started on port ${port}`)
-})
+    console.log(`Server has been started on port ${port}`);
+});
 
 
